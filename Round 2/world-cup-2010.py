830d0035d32352cf0df9cb9ab950416b1e517134
Allen Liu <kamyu104@gmail.com>
2018-06-23 18:37:20 +0800

    Create world-cup-2010.py

---
+++ Round 2/world-cup-2010.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2018 kamyu. All rights reserved.
+#
+# Google Code Jam 2010 Round 2- Problem B. World Cup 2010
+# https://code.google.com/codejam/contest/635102/dashboard#s=p1&a=1
+#
+# Time:  O(P^2)
+# Space: O(P^2)
+#
+
+def world_cup_2010():
+    P = input()
+    M = map(int, raw_input().strip().split())
+    M.reverse()
+    cost = []
+    for _ in xrange(P):
+        cost.extend(map(int, raw_input().strip().split()))
+    cost.reverse()
+
+    dp = [[float("inf") for _ in xrange(P+2)] for _ in xrange(2**(P+1)-1)]
+    half = 2**P-1
+    for i in reversed(xrange(2**(P+1)-1)):
+        for j in reversed(xrange(P+1)):
+            if i >= half:
+                if j <= M[i-half]:
+                    dp[i][j] = 0
+            else:
+                dp[i][j] = min(dp[i][j],
+                               min(dp[i*2+1][j+1] + dp[i*2+2][j+1],
+                                   dp[i*2+1][j] + dp[i*2+2][j] + cost[i]))
+    return dp[0][0]
+    
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, world_cup_2010())
