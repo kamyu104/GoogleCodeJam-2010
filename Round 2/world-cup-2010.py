75f51b0990dce38f2463a5620a404378dccaa55f
Allen Liu <kamyu104@gmail.com>
2018-06-23 19:31:38 +0800

    update

--- Round 2/world-cup-2010.py
+++ Round 2/world-cup-2010.py
@@ -14,15 +14,15 @@ def world_cup_2010():
         cost.extend(map(int, raw_input().strip().split()))
     cost.reverse()
 
-    dp = [[float("inf") for _ in xrange(P+2)] for _ in xrange(2**(P+1)-1)]
+    dp = [[float("inf") for _ in xrange(P+1)] for _ in xrange(2**(P+1)-1)]
     for i in reversed(xrange(2**(P+1)-1)):
-        for j in reversed(xrange(P+1)):
-            if i >= 2**P-1:
-                if j >= P-cost[i]:
-                    dp[i][j] = 0
-            else:
-                dp[i][j] = min(cost[i] + dp[i*2+1][j+1] + dp[i*2+2][j+1],
-                               dp[i*2+1][j] + dp[i*2+2][j])
+        if i >= 2**P-1:
+            for j in reversed(xrange(P-cost[i], P+1)):
+                dp[i][j] = 0
+            continue
+        for j in reversed(xrange(P)):
+            dp[i][j] = min(cost[i] + dp[i*2+1][j+1] + dp[i*2+2][j+1],
+                            dp[i*2+1][j] + dp[i*2+2][j])
     return dp[0][0]
     
 for case in xrange(input()):
